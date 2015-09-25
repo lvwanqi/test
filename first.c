a842f5e62a56da74bdb8bfa9833f110052708983
emily <emily.lv@cn.nextlabs.com>
2015-09-25 20:56:09 +0800

    first version

---
+++ first.c
@@ -0,0 +1,24 @@
+#include <ntddk.h>
+
+
+void DriverUnload(PDRIVER_OBJECT driver)
+{
+	DbgPrint("exit hello world\r\n");
+}
+
+
+NTSTATUS 
+  DriverEntry( 
+    IN PDRIVER_OBJECT  DriverObject, 
+    IN PUNICODE_STRING  RegistryPath 
+    )
+{
+#if DBG
+       _asm int 3
+#endif
+
+	DbgPrint("hello world!\r\n");
+	DriverObject->DriverUnload=DriverUnload;
+	return STATUS_SUCCESS;
+
+}

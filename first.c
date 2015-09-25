fc8b8487b40744e80fc0058d2f7eb8ecece1c5f8
emily <emily.lv@cn.nextlabs.com>
2015-09-25 21:19:36 +0800

    uuuu

--- first.c
+++ first.c
@@ -4,15 +4,15 @@
 void DriverUnload(PDRIVER_OBJECT driver)
 {
 	DbgPrint("exit hello world\r\n");
-}
+}333333333333333
 
 
-NTSTATUS 
+NTSTATUS 33333333333333
   DriverEntry( 
     IN PDRIVER_OBJECT  DriverObject, 
     IN PUNICODE_STRING  RegistryPath 
     )
-{
+{33333333333333333
 #if DBG
        _asm int 3
 #endif
